d1e78741085d4933725b4a2e55f3c8b73bd5799a
miseongk <39994337+miseongk@users.noreply.github.com>
2021-06-12 15:37:15 +0900

    Fix app.js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const bodyparser = require("body-parser");
+
 const app = express();
-const port = 3000;
+const API_PORT = process.env.API_PORT || 3000;
+const API_HOST = process.env.API_HOST || "localhost";
+const API_ROOT = "/api/v1/";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const auth = require("./api/user/auth");
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.use(API_ROOT, auth);
+
+app.listen(API_PORT, API_HOST, () => {
+  console.log(
+    `Sejong Mentoring System running at http://${API_HOST}:${API_PORT}`
+  );
 });
