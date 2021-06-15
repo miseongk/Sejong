b30097fabd51b2753f74b7c6fc1b3149a97fae9a
miseongk <39994337+miseongk@users.noreply.github.com>
2021-06-16 01:27:31 +0900

    Fix error

--- api/chat/utils.js
+++ api/chat/utils.js
@@ -5,7 +5,7 @@ async function findRoom(user1, user2) {
     `SELECT room_id FROM ChatRoom 
         WHERE (user1=${user1} AND user2=${user2}) OR (user1=${user2} AND user2=${user1}); `
   );
-  return room;
+  return room[0].room_id;
 }
 
 module.exports = { findRoom };
