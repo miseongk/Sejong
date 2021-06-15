786d5af0137840f749da2106e2e5115c5962a502
miseongk <39994337+miseongk@users.noreply.github.com>
2021-06-15 21:13:41 +0900

    server test

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
 const bodyparser = require("body-parser");
-
+//require("dotenv").config();
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const API_PORT = process.env.API_PORT || 3000;
 const API_HOST = process.env.API_HOST || "localhost";
 const API_ROOT = "/api/v1/";
 
 const auth = require("./api/user/auth");
+const chat = require("./api/chat/chat");
+const middleware = require("./api/middleware/middleware");
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
+app.use(middleware._log);
 
 app.use(API_ROOT, auth);
+app.use(API_ROOT, chat);
 
+require("./api/chat/socket")(app, io);
+
+app.get("/", (req, res) => {
+  res.send("hello!!");
+});
 app.listen(API_PORT, API_HOST, () => {
   console.log(
     `Sejong Mentoring System running at http://${API_HOST}:${API_PORT}`
   );
 });
+
+// if (process.env.NODE_ENV === "production") {
+//   dotenv.config(".env");
+// } else if (process.env.NODE_ENV === "develop") {
+//   dotenv.config(".env.dev");
+// } else {
+//   throw new Error("process.env.NODE_ENV를 설정하지 않았습니다!");
+// }
