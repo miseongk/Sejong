c5713e353e6215837527ba8c6e99108223f83853
miseongk <39994337+miseongk@users.noreply.github.com>
2021-06-16 01:26:12 +0900

    Add chat API

--- api/chat/chat.js
+++ api/chat/chat.js
@@ -1,32 +1,59 @@
-const moment = require("moment");
+const express = require("express");
+const router = express.Router();
+
 const _query = require("../../database/db");
-const findRoom = require("./utils");
+const middleware = require("../middleware/middleware");
+const { findRoom } = require("./utils");
 
-module.exports = (app, io) => {
-  io.on("connection", (socket) => {
-    console.log("user connected: ", socket.id);
+//Get chatting lists
+router.get("/chat", middleware._auth, async (req, res) => {
+  let query_response = {};
 
-    socket.on("joinRoom", async ({ user1, user2 }) => {
-      const room = findRoom(user1, user2);
-      if (room.length == 0) {
-        await _query(
-          `INSERT INTO ChatRoom (user1, user2) VALUES (${user1},${user2});`
-        );
-        room = findRoom(user1, user2);
-      }
-      socket.join(room);
-    });
+  const user = res.locals.student_id;
+  //const page = req.query.page - 1;
+  //const limit = 10;
+  //select content, time from chat where room_id in (select room_id from chatroom where user1=18000000 or user2=18000000) order by time desc limit 0,1;
+  let chatRoom = await _query(
+    `SELECT room_id, user1, user2 FROM ChatRoom WHERE user1=${user} OR user2=${user};`
+  );
+  if (chatRoom.length == 0) {
+    query_response.message = `User: ${user} doesn't have any chatting rooms`;
+    return res.send(query_response);
+  }
+  //user, content, time
+  for (let i = 0; i < chatRoom.length; i++) {
+    const chatList = await _query(
+      `SELECT content, time FROM Chat WHERE room_id=${chatRoom[i].room_id}
+          ORDER BY time desc LIMIT 0,1;`
+    );
+    chatRoom[i].content = chatList[0].content;
+    chatRoom[i].time = chatList[0].time;
+    if (chatRoom[i].user1 == user) {
+      chatRoom[i].room = chatRoom[i].user2;
+    } else {
+      chatRoom[i].room_name = chatRoom[i].user1;
+    }
+  }
+  query_response.data = chatRoom;
+  res.send(query_response);
+});
 
-    socket.on("chatMessage", async ({ msg, sender, receiver }) => {
-      const room = findRoom(sender, receiver);
-      await _query(
-        `INSERT INTO Chat (room_id, sender, content) VALUES (${room}, '${sender}', '${msg}');`
-      );
-      io.to(room).emit("message", { sender, msg, time: moment() });
-    });
+//Get messages
+router.get("/chat/:room_name", middleware._auth, async (req, res) => {
+  let query_response = {};
 
-    socket.on("disconnect", () => {
-      console.log("user disconnected: ", socket.id);
-    });
-  });
-};
+  const page = req.query.page - 1;
+  const limit = 10;
+  const user = res.locals.student_id;
+  const room_name = req.params.room_name;
+  const room_id = await findRoom(user, room_name);
+
+  query_response.data = await _query(
+    `SELECT id, sender, content, time, is_checked FROM Chat WHERE room_id=${room_id} 
+    ORDER BY time desc LIMIT ${page * limit}, ${limit};`
+  );
+
+  res.send(query_response);
+});
+
+module.exports = router;
