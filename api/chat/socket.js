fabe5384468b33b845ca385133d8ce0e913ce8e2
miseongk <39994337+miseongk@users.noreply.github.com>
2021-06-16 01:26:22 +0900

    Fix error

--- api/chat/socket.js
+++ api/chat/socket.js
@@ -2,25 +2,25 @@ const express = require("express");
 const moment = require("moment");
 
 const _query = require("../../database/db");
-const findRoom = require("./utils");
+const { findRoom } = require("./utils");
 
 module.exports = (app, io) => {
   io.on("connection", (socket) => {
     console.log("user connected: ", socket.id);
 
     socket.on("joinRoom", async ({ user1, user2 }) => {
-      const room = findRoom(user1, user2);
+      const room = await findRoom(user1, user2);
       if (room.length == 0) {
         await _query(
           `INSERT INTO ChatRoom (user1, user2) VALUES (${user1},${user2});`
         );
-        room = findRoom(user1, user2);
+        room = await findRoom(user1, user2);
       }
       socket.join(room);
     });
 
     socket.on("chatMessage", async ({ msg, sender, receiver }) => {
-      const room = findRoom(sender, receiver);
+      const room = await findRoom(sender, receiver);
       await _query(
         `INSERT INTO Chat (room_id, sender, content) VALUES (${room}, ${sender}, '${msg}');`
       );
