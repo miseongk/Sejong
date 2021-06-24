114e71f9821a8c4ce2955351794208ef3129b634
miseongk <39994337+miseongk@users.noreply.github.com>
2021-06-25 03:31:38 +0900

    Fix put profile API

--- api/user/profile.js
+++ api/user/profile.js
@@ -4,20 +4,27 @@ const router = express.Router();
 const _query = require("../../database/db");
 const middleware = require("../middleware/middleware");
 
-// Set gender
+// Set profile(gender, bio)
 router.put("/profile/:student_id", middleware._auth, async (req, res) => {
   let query_response = {};
 
   const gender = req.body.gender;
+  const bio = req.body.bio;
+
+  if (gender.length == 0 || bio.length == 0) {
+    res.status(400);
+    query_response.message = "There are empty requests.";
+    return res.send(query_response);
+  }
 
   try {
     await _query(
-      `UPDATE User SET gender = '${gender}' WHERE student_id = ${req.params.student_id};`
+      `UPDATE User SET gender = '${gender}', bio = '${bio}' WHERE student_id = ${req.params.student_id};`
     );
-    query_response.message = `Set ${req.params.student_id}'s gender successfully.`;
+    query_response.message = `Set ${req.params.student_id}'s profile successfully.`;
   } catch (error) {
     res.status(400);
-    query_response.message = `Failed to set ${req.params.student_id}'s gender.`;
+    query_response.message = `Failed to set ${req.params.student_id}'s profile.`;
   }
 
   res.send(query_response);
