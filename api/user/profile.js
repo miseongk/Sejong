931813c08a58408aad42e8debae15fb8ad3e812f
miseongk <39994337+miseongk@users.noreply.github.com>
2021-06-25 03:42:11 +0900

    Add get profile API

--- api/user/profile.js
+++ api/user/profile.js
@@ -30,4 +30,26 @@ router.put("/profile/:student_id", middleware._auth, async (req, res) => {
   res.send(query_response);
 });
 
+// Get profile
+router.get("/profile/:student_id", middleware._auth, async (req, res) => {
+  let query_response = {};
+
+  try {
+    const profile = await _query(
+      `SELECT student_id, name, major, gender, bio, reputation FROM User WHERE student_id = ${req.params.student_id};`
+    );
+    if (profile.length == 0) {
+      res.status(400);
+      query_response.message = `Student ID : '${req.params.student_id}' doesn't exist.`;
+      return res.send(query_response);
+    }
+    query_response.data = profile;
+  } catch (error) {
+    res.status(400);
+    query_response.message = `Failed to get ${req.params.student_id}'s profile.`;
+  }
+
+  res.send(query_response);
+});
+
 module.exports = router;
