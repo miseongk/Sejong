74aa913b81cd5c109d8a6aa3b504aeda899f55ab
miseongk <39994337+miseongk@users.noreply.github.com>
2021-06-26 00:32:26 +0900

    Add get posts and set bio API

--- api/user/profile.js
+++ api/user/profile.js
@@ -52,4 +52,53 @@ router.get("/profile/:student_id", middleware._auth, async (req, res) => {
   res.send(query_response);
 });
 
+// Get a specific user's posts
+router.get("/profile/:student_id/posts", middleware._auth, async (req, res) => {
+  let query_response = {};
+
+  try {
+    const posts = await _query(
+      `SELECT * FROM Post WHERE student_id = ${req.params.student_id} ORDER BY updated_at desc;`
+    );
+    if (posts.length == 0) {
+      res.status(400);
+      query_response.message = `Student ID : '${req.params.student_id}' doesn't exist.`;
+      return res.send(query_response);
+    }
+    query_response.data = posts;
+  } catch (error) {
+    res.status(400);
+    query_response.message = `Failed to get ${req.params.student_id}'s posts.`;
+  }
+
+  res.send(query_response);
+});
+
+// Set bio
+router.put("/profile/:student_id/bio", middleware._auth, async (req, res) => {
+  let query_response = {};
+
+  const bio = req.body.bio;
+
+  try {
+    const student = await _query(
+      `SELECT * FROM User WHERE student_id = ${req.params.student_id};`
+    );
+    if (student.length == 0) {
+      res.status(400);
+      query_response.message = `Student ID: '${req.params.student_id}' doesn't exist.`;
+      return res.send(query_response);
+    }
+    await _query(
+      `UPDATE User SET bio = '${bio}' WHERE student_id = ${req.params.student_id};`
+    );
+    query_response.message = `Set ${req.params.student_id}'s bio successfully.`;
+  } catch (error) {
+    res.status(400);
+    query_response.message = `Failed to set ${req.params.student_id}'s bio.`;
+  }
+
+  res.send(query_response);
+});
+
 module.exports = router;
