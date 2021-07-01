304aed8780a3b12f2b9ffb7e5528a9f8d9e55fc7
miseongk <39994337+miseongk@users.noreply.github.com>
2021-07-02 04:32:41 +0900

    Fix findRoom function

--- api/chat/utils.js
+++ api/chat/utils.js
@@ -1,11 +1,11 @@
 const _query = require("../../database/db");
 
-async function findRoom(user1, user2) {
+async function findRoom(user1, user2, post) {
   const room = await _query(
     `SELECT room_id FROM ChatRoom 
-        WHERE (user1=${user1} AND user2=${user2}) OR (user1=${user2} AND user2=${user1}); `
+        WHERE (user1=${user1} AND user2=${user2}) OR (user1=${user2} AND user2=${user1}) AND post = ${post}; `
   );
-  return room[0].room_id;
+  return room;
 }
 
 module.exports = { findRoom };
