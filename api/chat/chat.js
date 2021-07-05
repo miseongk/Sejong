2507ec34dc8435728cc6336d63e13e919bc758d5
miseongk <39994337+miseongk@users.noreply.github.com>
2021-07-05 23:46:13 +0900

    Fix get chatting list error

--- api/chat/chat.js
+++ api/chat/chat.js
@@ -34,38 +34,41 @@ router.get("/chat", middleware._auth, async (req, res) => {
           ORDER BY time desc LIMIT 0,1;`
       );
       if (chatList.length == 0) {
+        chatRoom.splice(i, 1);
+        i--;
         continue;
-      }
-      const is_matched = await _query(
-        `SELECT is_matched FROM Post WHERE id IN (SELECT post FROM ChatRoom WHERE room_id=${chatRoom[i].room_id});`
-      );
-      chatRoom[i].msg = chatList[0].content;
-      chatRoom[i].time = chatList[0].time;
-      chatRoom[i].is_matched = is_matched[0].is_matched;
-      // role, subject, content
-      const post_info = await _query(
-        `SELECT id, name, role, subject, content FROM Post WHERE id IN (SELECT post FROM ChatRoom WHERE room_id=${chatRoom[i].room_id});`
-      );
-      chatRoom[i].post_id = post_info[0].id;
-      chatRoom[i].post_name = post_info[0].name;
-      if (post_info[0].role == 1) {
-        chatRoom[i].role = "멘토";
       } else {
-        chatRoom[i].role = "멘티";
-      }
-      chatRoom[i].subject = post_info[0].subject;
-      chatRoom[i].content = post_info[0].content;
-      let user_name = "";
-      if (chatRoom[i].user1 == user) {
-        user_name = await _query(
-          `SELECT name FROM User WHERE student_id = ${chatRoom[i].user2};`
+        const is_matched = await _query(
+          `SELECT is_matched FROM Post WHERE id IN (SELECT post FROM ChatRoom WHERE room_id=${chatRoom[i].room_id});`
         );
-      } else {
-        user_name = await _query(
-          `SELECT name FROM User WHERE student_id = ${chatRoom[i].user1};`
+        chatRoom[i].msg = chatList[0].content;
+        chatRoom[i].time = chatList[0].time;
+        chatRoom[i].is_matched = is_matched[0].is_matched;
+        // role, subject, content
+        const post_info = await _query(
+          `SELECT id, name, role, subject, content FROM Post WHERE id IN (SELECT post FROM ChatRoom WHERE room_id=${chatRoom[i].room_id});`
         );
+        chatRoom[i].post_id = post_info[0].id;
+        chatRoom[i].post_name = post_info[0].name;
+        if (post_info[0].role == 1) {
+          chatRoom[i].role = "멘토";
+        } else {
+          chatRoom[i].role = "멘티";
+        }
+        chatRoom[i].subject = post_info[0].subject;
+        chatRoom[i].content = post_info[0].content;
+        let user_name = "";
+        if (chatRoom[i].user1 == user) {
+          user_name = await _query(
+            `SELECT name FROM User WHERE student_id = ${chatRoom[i].user2};`
+          );
+        } else {
+          user_name = await _query(
+            `SELECT name FROM User WHERE student_id = ${chatRoom[i].user1};`
+          );
+        }
+        chatRoom[i].room_name = user_name[0].name;
       }
-      chatRoom[i].room_name = user_name[0].name;
     }
     query_response.data = chatRoom.sort(date_descending);
   } catch (error) {
@@ -101,27 +104,5 @@ router.get("/chat/:post_id/:student_id", middleware._auth, async (req, res) => {
 
   res.send(query_response);
 });
-
-// // Get messages
-// router.get("/chat/:room_id", middleware._auth, async (req, res) => {
-//   let query_response = {};
-
-//   const page = req.query.page - 1;
-//   const limit = 10;
-//   //const user = res.locals.student_id;
-//   const room_id = req.params.room_id;
-
-//   try {
-//     query_response.data = await _query(
-//       `SELECT id, sender, content, time, is_checked FROM Chat WHERE room_id=${room_id}
-//       LIMIT ${page * limit}, ${limit};`
-//     );
-//   } catch (error) {
-//     res.status(400);
-//     query_response.message = "Failed to get messages.";
-//   }
-
-//   res.send(query_response);
-// });
 
 module.exports = router;
