4f60b0844dcb21554f4240e21fa72bc52e4b34dd
miseongk <39994337+miseongk@users.noreply.github.com>
2021-07-05 18:58:48 +0900

    Fix get chatting list API

--- api/chat/chat.js
+++ api/chat/chat.js
@@ -41,9 +41,10 @@ router.get("/chat", middleware._auth, async (req, res) => {
       chatRoom[i].is_matched = is_matched[0].is_matched;
       // role, subject, content
       const post_info = await _query(
-        `SELECT id, role, subject, content FROM Post WHERE id IN (SELECT post FROM ChatRoom WHERE room_id=${chatRoom[i].room_id});`
+        `SELECT id, name, role, subject, content FROM Post WHERE id IN (SELECT post FROM ChatRoom WHERE room_id=${chatRoom[i].room_id});`
       );
       chatRoom[i].post_id = post_info[0].id;
+      chatRoom[i].post_name = post_info[0].name;
       if (post_info[0].role == 1) {
         chatRoom[i].role = "멘토";
       } else {
