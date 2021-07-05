deba9108ee11d198b0173374166a0c7991bbd577
miseongk <39994337+miseongk@users.noreply.github.com>
2021-07-05 19:58:46 +0900

    Fix query error

--- api/chat/utils.js
+++ api/chat/utils.js
@@ -3,7 +3,7 @@ const _query = require("../../database/db");
 async function findRoom(user1, user2, post) {
   const room = await _query(
     `SELECT room_id FROM ChatRoom 
-        WHERE (user1=${user1} AND user2=${user2}) OR (user1=${user2} AND user2=${user1}) AND post = ${post}; `
+        WHERE ((user1=${user1} AND user2=${user2}) OR (user1=${user2} AND user2=${user1})) AND post = ${post}; `
   );
   return room;
 }
